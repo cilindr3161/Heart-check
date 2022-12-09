da66522bd65ea4a438f0297a1a7f35ba4f4d38da
cilindr3161 <118201473+cilindr3161@users.noreply.github.com>
2022-12-09 20:48:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from PyQT5 import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+ 
+from instr import *
+from second_win import *
+     
+class MainWin(QWidget):
+   def __init__(self):
+       ''' окно, в котором располагается приветствие '''
+       super().__init__()
